010df3be27d266fe35b00d5ef493f387146f2813
Hsu <edittye379@gmail.com>
2016-12-14 03:13:12 +0800

    header不能有底線r

--- app/Http/Middleware/ApiTokenMiddleware.php
+++ app/Http/Middleware/ApiTokenMiddleware.php
@@ -6,11 +6,6 @@ use Closure;
 
 class ApiTokenMiddleware
 {
-
-    protected $except = [
-        'api/test'
-    ];
-
     /**
      * Handle an incoming request.
      *
@@ -21,7 +16,7 @@ class ApiTokenMiddleware
     public function handle($request, Closure $next)
     {
         // var_dump(config('api.token'));
-        $header = $request->header('api_token');
+        $header = $request->header('apitoken');
         if (!$header || $header != config('api.token')) {
             //return 'invalid';
             \App::abort(401, 'unauthorication');
